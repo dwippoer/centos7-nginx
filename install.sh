1a3f2ecc5f00f6b203f422e323d4b4d5bdb15653
Dwi Purnomo <dwippoer@gmail.com>
2017-02-16 13:18:23 +0700

    update installer

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+dir1_nginx=/etc/nginx/conf.d
+
 install_nginx()
 {
 	sudo yum -y install nginx
 	sudo systemctl start nginx && sudo systemctl enable nginx
 }
-install_nginx 
+install_nginx
+
+#check dir nginx/conf.d
+if [ -d /etc/nginx/conf.d ];
+then
+	sudo wget https://github.com/dwippoer/centos7-nginx/blob/master/vhost1.conf -P $dir1_nginx;
+else
+	sudo mkdir $dir1_nginx && sudo wget https://github.com/dwippoer/centos7-nginx/blob/master/vhost1.conf -P $dir1_nginx;
+fi
+
+#update nginx
+sudo rm -f /etc/nginx/nginx.conf && sudo wget https://github.com/dwippoer/centos7-nginx/blob/master/nginx.conf -P /etc/nginx
+
+
